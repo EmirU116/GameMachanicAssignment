4df04e0005aeaf8ec67388c594ead67de014ce0e
EmirU116 <emir.ulusoy@student.forsbergsskola.se>
2022-03-22 11:05:18 +0100

    Setup Scene
    Created Ground and Box as Enviorments
    
    Created a PlayerController Script

---
+++ MyGameMachanic/Assets/Scripts/PlayerScript/PlayerController.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody myRigidbody;
+    public float moveSpeed = -5f;
+    public float jumpforce = 500f;
+
+    private void Update()
+    {
+        //Get move input
+        //Preferably get input in Update()
+        var moveInput = Input.GetAxis("Horizontal");
+        
+        //Set move velocity
+        //Preferably interact with physics in FixedUpdate()
+        myRigidbody.velocity = new Vector3(moveInput = moveSpeed, myRigidbody.velocity.y, 0);
+        
+        //Get jump input
+        //Preferably get input in Update()
+        var jumpInput = Input.GetKeyDown(KeyCode.Space);
+        
+        //Apply jump force
+        //Preferably interact with physics in FixedUpdate()
+        if (jumpInput)
+        {
+            myRigidbody.AddForce(Vector3.up * jumpforce);
+        }
+    }
+}
