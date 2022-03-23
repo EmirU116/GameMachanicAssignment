767ff06b348a116207291cdb23064101ae361e01
EmirU116 <emir.ulusoy@student.forsbergsskola.se>
2022-03-23 16:10:19 +0100

    Explination on what do code does

--- MyGameMachanic/Assets/Scripts/PlayerScript/ThridpersonMovement.cs
+++ MyGameMachanic/Assets/Scripts/PlayerScript/ThridpersonMovement.cs
@@ -8,30 +8,30 @@ public class ThridpersonMovement : MonoBehaviour
     public CharacterController controller;      //Motor that drives our player
 
     public float speed = 6f;        //Movement speed
-    private Vector3 mVelocity;
+    private Vector3 mVelocity;      //Movemnet
 
     // Update is called once per frame
     void Update()
     {
-        mVelocity = Vector3.zero;
+        mVelocity = Vector3.zero;       //Cleaner code for Vector3(0,0,0)
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(0f,0f,speed*Time.deltaTime);
+            transform.Translate(0f,0f,speed*Time.deltaTime);        // Forward Movement
             mVelocity.z = 1.0f;
-        } else if (Input.GetKey(KeyCode.S))
+        } else if (Input.GetKey(KeyCode.S))     // Backward Movement
         {
             transform.Translate(0f,0f, -speed*Time.deltaTime);
             mVelocity.z = -1.0f;
         }
 
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D))        // Rotating to the right
         {
             transform.Rotate(0f, 120*Time.deltaTime, 0f);
-        }else if (Input.GetKey(KeyCode.A))
+        }else if (Input.GetKey(KeyCode.A))      //  Rotating to the left
         {
             transform.Rotate(0f, -120*Time.deltaTime, 0f);
         }
         
-        transform.Translate(mVelocity.normalized * Time.deltaTime * speed);
+        transform.Translate(mVelocity.normalized * Time.deltaTime * speed);     // This code is for moving the same speed at any direction
     }
 }
