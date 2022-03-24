d099c080ade7d433834a5391227db7cea298bf3e
EmirU116 <emir.ulusoy@student.forsbergsskola.se>
2022-03-24 16:00:28 +0100

    Character can move by the Z axis,
    It works but not happy yet with it

--- MyGameMachanic/Assets/Scripts/PlayerScript/PlayerMovement.cs
+++ MyGameMachanic/Assets/Scripts/PlayerScript/PlayerMovement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Rigidbody myRigidbody;       // Field for position through physics simulation
+    public float moveSpeed = 5f;        // Field for Movement
+    public float jumpForce = 500f;      // Field for Jumping         
 
     // Start is called before the first frame update
     void Start()
@@ -14,6 +17,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Get move Input
+        //Preferably Get Input in Update()
+        //Assigning the Horizontal Movement
+        var moveInput = Input.GetAxis("Vertical");
+
+        myRigidbody.velocity = new Vector3(0, myRigidbody.velocity.y, moveInput * moveSpeed);
     }
 }
