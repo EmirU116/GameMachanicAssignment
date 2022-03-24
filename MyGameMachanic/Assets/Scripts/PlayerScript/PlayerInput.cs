1be71f86e586b522ea034ea3baa4b0169208b88d
EmirU116 <emir.ulusoy@student.forsbergsskola.se>
2022-03-25 00:57:50 +0100

    More Sorting Scripts
    Created a PlayerInput scripts to only put the inputs like jumping and movement

---
+++ MyGameMachanic/Assets/Scripts/PlayerScript/PlayerInput.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    private bool jumpInput;
+    private float moveInputV;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get jump input
+        //Preferably get input in Update()
+        jumpInput = Input.GetKeyDown(KeyCode.Space);
+
+        //Get move Input
+        //Preferably Get Input in Update()
+        //Assigning the Horizontal Movement
+        moveInputV = Input.GetAxis("Horizontal");
+    }
+}
