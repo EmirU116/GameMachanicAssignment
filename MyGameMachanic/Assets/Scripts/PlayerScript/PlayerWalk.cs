d860fb12800de96825b2e7b9826dd45f5e0ef312
EmirU116 <emir.ulusoy@student.forsbergsskola.se>
2022-03-29 17:57:24 +0200

    Started with dash Cooldown
    i got it to work a little bit, i can dash 2 or 3 times but then i cant, the problem is, is that the timer dosent count down

--- MyGameMachanic/Assets/Scripts/PlayerScript/PlayerWalk.cs
+++ MyGameMachanic/Assets/Scripts/PlayerScript/PlayerWalk.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerWalk : MonoBehaviour
@@ -9,25 +10,38 @@ public class PlayerWalk : MonoBehaviour
     [SerializeField] private PlayerInput PI;      // Accessing Manually another script
     [SerializeField] private float normalSpeed = 5f;
 
+    public float dashCooldown;
+
     private PlayerInput Pi;
 
     [SerializeField] private float dashLenght = 10f;
     // Update is called once per frame
     void Update()
     {
+        
+        
         //Set move velocity
         //Preferably interact with physics in FixedUpdate()
         myRigidbody.velocity = new Vector3(PI.MoveInput * moveSpeed, myRigidbody.velocity.y, 0);
         
         // Dash Machanic Basic
+        
+        
         if (Input.GetKeyDown(KeyCode.R))
         {
-            moveSpeed = 20F;
-            transform.Translate(0,0,dashLenght * PI.MoveInput);
+            dashCooldown = Time.time;
+            
+            if (dashCooldown <= 4 )
+            {
+                moveSpeed = 20F;
+                transform.Translate(0,0,dashLenght * PI.MoveInput);
+            }
+                        
         }
         else
         {
             moveSpeed = normalSpeed;
         }
+
     }
 }
