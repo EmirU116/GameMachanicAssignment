99c9cfd256ba8012e84bfcf940e3adccc793635f
EmirU116 <emir.ulusoy@student.forsbergsskola.se>
2022-03-29 15:07:14 +0200

    Dash Update
    Got the dash now to work, so when your press dash button, it will teleport/dash to the only left side, but after i dash i few times, i cant jump at all, if i only jump and not dash at all, i can jump as much as possible

--- MyGameMachanic/Assets/Scripts/PlayerScript/PlayerWalk.cs
+++ MyGameMachanic/Assets/Scripts/PlayerScript/PlayerWalk.cs
@@ -6,20 +6,22 @@ public class PlayerWalk : MonoBehaviour
 {
     [SerializeField] private Rigidbody myRigidbody;       // Field for position through physics simulation, also accesing the Rigidbody script
     [SerializeField] private float moveSpeed = 5f;        // Field for Movement
-
     [SerializeField] private PlayerInput PI;      // Accessing Manually another script
-
     [SerializeField] private float normalSpeed = 5f;
+
+    [SerializeField] private float dashLenght = 10f;
     // Update is called once per frame
     void Update()
     {
         //Set move velocity
         //Preferably interact with physics in FixedUpdate()
         myRigidbody.velocity = new Vector3(PI.MoveInput * moveSpeed, myRigidbody.velocity.y, 0);
-
-        if (Input.GetKey(KeyCode.R))
+        
+        // Dash Machanic Basic
+        if (Input.GetKeyDown(KeyCode.R))
         {
             moveSpeed = 20F;
+            transform.Translate(0,0,-dashLenght);
         }
         else
         {
